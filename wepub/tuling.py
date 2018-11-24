db64aed2afebfad0d370335fb74a79178c077b5d
hallucigenia <air.bylive@gmail.com>
2018-11-24 20:17:51 +0800

    debug tuling

--- wepub/tuling.py
+++ wepub/tuling.py
@@ -8,30 +8,13 @@ import requests
 TULING_KEY = os.getenv('TULING_KEY')
 
 def get_response(openid, msg):
-    api = 'http://openapi.tuling123.com/openapi/api/v2'
-    dat = {
-        "perception": {
-            "inputText": {
-                "text": msg
-            },
-            "inputImage": {
-                "url": "imageUrl"
-            },
-            "selfInfo": {
-                "location": {
-                    "city": "北京",
-                    "province": "北京",
-                    "street": "信息路"
-                }
-            }
-        },
-        "userInfo": {
-            "apiKey": TULING_KEY,
-            "userId": openid
-        }
-    }
-    dat = json.dumps(dat)
-    r = requests.post(api, data=dat).json()
-
-    message = r['results'][0]['values']['text']
-    return message
+    inputText = {'text': msg}
+    userInfo = {'apiKey': TULING_KEY, 'userId': openid}
+    perception = {'inputText': inputText}
+    data = {'perception': perception, 'userInfo': userInfo}
+    url = 'http://openapi.tuling123.com/openapi/api/v2'
+    response = requests.post(url=url, data=json.dumps(data))
+    response.encoding = 'utf-8'
+    result = response.json()
+    answer = result['results'][0]['values']['text']
+    return answer
